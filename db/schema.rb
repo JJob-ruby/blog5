8f3bc3b49551a1e3d7d58c2d5c6b9debff014cc4
JJob93 <cjoe8523@gmail.com>
2019-12-30 00:08:43 +0530

    Comment controller added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_29_121700) do
+ActiveRecord::Schema.define(version: 2019_12_29_182431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,19 @@ ActiveRecord::Schema.define(version: 2019_12_29_121700) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
   create_table "role", primary_key: "role_id", id: :serial, force: :cascade do |t|
     t.string "role_name", limit: 255, null: false
     t.index ["role_name"], name: "role_role_name_key", unique: true
   end
 
+  add_foreign_key "comments", "articles"
 end
