cbd4334ddb510c71c338f91359c7a1ed6b459272
JJob93 <cjoe8523@gmail.com>
2019-12-29 22:51:57 +0530

    CR part completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,15 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
+    @article= Article.new
   end
 
 =begin
@@ -12,8 +21,11 @@ class ArticlesController < ApplicationController
 def create
   @article = Article.new(article_params)
  
-  @article.save
-  redirect_to @article
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+  end
 end
 
 private
